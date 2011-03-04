fa9bd0340ea964f8bf93ad793d7e6ff33aa4dc58
ssp <porst@sub.uni-goettingen.de>
2011-03-04 14:47:42 +0100

    script and XSL for processing PicaXML

---
+++ PicaXML-to-singleRecords.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#coding=utf-8
+"""
+Input: a big XML file containing Pica XML output of <record> elements.
+Output: TurboMac Pica XML with one file per record. The file name is PPN.xml.
+"""
+import sys
+from lxml import etree
+import json
+
+filename = sys.argv[1]
+
+xslXML2 = etree.parse("/home/ssp/bin/XSLT/Remove-Namespaces.xsl")
+xsl2 = etree.XSLT(xslXML2)
+
+xslXML = etree.parse("/home/ssp/bin/XSLT/MarcXML2TurboMarc.xsl")
+xsl = etree.XSLT(xslXML)
+
+
+xml = etree.parse(filename)
+
+
+xml = xsl2(xml)
+xml = xsl(xml)
+xml = xsl2(xml)
+
+records = xml.xpath("//r")
+	
+
+for record in records:
+	# Pica record
+	PPNField = record.xpath(u"//d003Ä/s0")
+	id = None
+	if len(PPNField) > 0:
+		id = PPNField[0].text
+	else:
+		# Marc Record
+		IDField = record.xpath("c001")
+		if len(IDField) > 0:
+			id = IDField[0].text
+
+	if id:
+		print id
+
+		XMLFile = open (id + ".xml", "w")
+		XMLFile.write(etree.tostring(record, encoding="utf-8", method="xml"))
+		XMLFile.close()
+
+	else:
+		print 'Could not process record.'
---
+++ XSLT/Remove-Namespaces.xsl
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--
+	Removes namespaces.
+
+	Found here:
+	http://wiki.tei-c.org/index.php/Remove-Namespaces.xsl
+-->
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:output method="xml" indent="no"/>
+
+<xsl:template match="/|comment()|processing-instruction()">
+    <xsl:copy>
+      <xsl:apply-templates/>
+    </xsl:copy>
+</xsl:template>
+
+<xsl:template match="*">
+    <xsl:element name="{local-name()}">
+      <xsl:apply-templates select="@*|node()"/>
+    </xsl:element>
+</xsl:template>
+
+<xsl:template match="@*">
+    <xsl:attribute name="{local-name()}">
+      <xsl:value-of select="."/>
+    </xsl:attribute>
+</xsl:template>
+</xsl:stylesheet>
