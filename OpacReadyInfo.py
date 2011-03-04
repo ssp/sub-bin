6e22017324e8f6ed76be3a5e55aac801fe6f2f59
ssp <porst@sub.uni-goettingen.de>
2011-03-04 14:47:02 +0100

    query for the work done by Fachreferenten

---
+++ OpacReadyInfo.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+import urllib2
+from lxml import etree
+from datetime import *
+import json
+
+name = 'ds'
+hitcountXPath = '/RESULT/SET/@hits'
+
+opacBaseURL = 'http://opac.sub.uni-goettingen.de/DB=1/XML=1/CMD?ACT=SRCHA&TRM=exk+ref+'
+opacURL = opacBaseURL + name
+
+xmlString = urllib2.urlopen(opacURL).read()
+xml = etree.fromstring(xmlString)
+
+allBooks = xml.xpath(hitcountXPath)[0]
+
+opacURL = opacBaseURL + name + '+AND+LKL+[ABCDEFGHIJKLMNOPQRSTUVWXYZ]*'
+
+xmlString = urllib2.urlopen(opacURL).read()
+xml = etree.fromstring(xmlString)
+
+readyBooks = xml.xpath(hitcountXPath)[0]
+
+data = {}
+data['person'] = name
+data['date'] = datetime.today().isoformat()
+data['allBooks'] = allBooks
+data['readyBooks'] = readyBooks
+
+couchurl = 'http://vlib.sub.uni-goettingen.de/couch/exk/'
+postdata = json.dumps(data)
+
+auth_handler = urllib2.HTTPBasicAuthHandler()
+auth_handler.add_password(None, 'http://vlib.sub.uni-goettingen.de/couch/exk', 'exk', 'exk')
+opener = urllib2.build_opener(auth_handler)
+req = urllib2.Request(couchurl)
+req.add_header('Content-Type', 'application/json')
+
+couchresult = opener.open(req, postdata)
+
